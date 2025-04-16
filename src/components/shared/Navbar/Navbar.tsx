cc713d1f4f9a3502ac5ac34b4016f4b81d90f195
Maisha Maliha Chowa <85276396+Maisha-Chowa@users.noreply.github.com>
2025-04-16 14:01:37 +0600

    project-setup-navbar-and-footer-ui

---
+++ src/components/shared/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <Container >
+           <Stack py={2} direction="row" justifyContent="space-between" alignItems='center'> 
+            <Typography variant='h4' fontWeight={600} component={Link} href="/">P
+                <Box component='span' color="primary.main">H</Box> {' '}Health Care</Typography>
+           
+             
+           <Stack direction='row' gap={4} justifyContent='space-between'>
+           <Typography component={Link} href="/consultation">Consultation</Typography>
+             <Typography component={Link} href="/health-plans">Health Plans</Typography>
+             <Typography component={Link} href="/medicine">Medicine</Typography>
+             <Typography component={Link} href="/diagnostics">Diagnostics</Typography>
+             <Typography component={Link} href="/ngo">NGOs</Typography>
+           </Stack>
+           <Button component={Link} href="/login"> Login</Button>
+           </Stack>
+        </Container>
+    );
+};
+
+export default Navbar;
