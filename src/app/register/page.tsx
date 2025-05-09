c8c811156ee888a1ff8f053e79d333f7f56adf26
Maisha Maliha Chowa <85276396+Maisha-Chowa@users.noreply.github.com>
2025-05-10 00:43:04 +0600

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,83 @@
-import React from 'react';
+"use client";
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import assests from "@/assets";
+import { styled } from "@mui/material/styles";
 
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 const Register = () => {
-    return (
-        <div>
-            Register
-        </div>
-    );
+  return (
+    <Container
+      sx={{
+        padding: "50px",
+      }}
+    >
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image
+                src={assests.svgs.logo}
+                width={50}
+                height={50}
+                alt="logo"
+              />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <Grid container spacing={2} m={1}>
+              <Grid size={12}>
+                <Item>
+                  <TextField
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Item>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Register;
