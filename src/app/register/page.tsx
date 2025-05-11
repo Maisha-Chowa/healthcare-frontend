72b8ae6a150a76b197cc84308addb300fc9c3a73
Maisha Maliha Chowa <85276396+Maisha-Chowa@users.noreply.github.com>
2025-05-11 08:50:04 +0600

    authentication-flow

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -14,13 +14,63 @@ import React from "react";
 import assests from "@/assets";
 import { styled } from "@mui/material/styles";
 import Link from "next/link";
+import { register } from "module";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { modifyPayload } from "@/utils/modifyPayload";
+import { registerPatient } from "@/services/actions/registerPatient";
+import { toast } from "sonner";
+import { userLogin } from "@/services/actions/userLogin";
+import { storeUserInfo } from "@/services/auth.services";
+import { useRouter } from "next/navigation";
 
-const Item = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   padding: theme.spacing(1),
+//   textAlign: "center",
+//   color: theme.palette.text.secondary,
+// }));
+
+interface IPatientData {
+  name: string;
+  email: string;
+  contactNumber: string;
+  address: string;
+}
+
+interface IPatientRegisterFormData {
+  password: string;
+  patient: IPatientData;
+}
 const Register = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IPatientRegisterFormData>();
+  const onSubmit: SubmitHandler<IPatientRegisterFormData> = async (
+    values: any
+  ) => {
+    console.log(values);
+    const data = modifyPayload(values);
+    try {
+      const res = await registerPatient(data);
+      console.log(res);
+      if (res?.data?.id) {
+        toast.success(res?.message);
+        const result = await userLogin({
+          password: values.password,
+          email: values.patient.email,
+        });
+        if (result?.data?.accessToken) {
+          storeUserInfo({ accessToken: result?.data?.accessToken });
+          router.push("/");
+        }
+      }
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
   return (
     <Container
       sx={{
@@ -63,73 +113,80 @@ const Register = () => {
             </Box>
           </Stack>
           <Box>
-            <Grid container spacing={2} m={1}>
-              <Grid size={{ xs: 12 }}>
-                <TextField
-                  label="Name"
-                  type="text"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                  required
-                />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Grid container spacing={2} m={1}>
+                <Grid size={{ xs: 12 }}>
+                  <TextField
+                    label="Name"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    required
+                    {...register("patient.name")}
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <TextField
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    required
+                    {...register("patient.email")}
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    required
+                    {...register("password")}
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <TextField
+                    label="Contact Number"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    required
+                    {...register("patient.contactNumber")}
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <TextField
+                    label="Address"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                    required
+                    {...register("patient.address")}
+                  />
+                </Grid>
               </Grid>
-              <Grid size={{ xs: 12, sm: 6 }}>
-                <TextField
-                  label="Email"
-                  type="email"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                  required
-                />
-              </Grid>
-              <Grid size={{ xs: 12, sm: 6 }}>
-                <TextField
-                  label="Password"
-                  type="password"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                  required
-                />
-              </Grid>
-              <Grid size={{ xs: 12, sm: 6 }}>
-                <TextField
-                  label="Contact Number"
-                  type="text"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                  required
-                />
-              </Grid>
-              <Grid size={{ xs: 12, sm: 6 }}>
-                <TextField
-                  label="Address"
-                  type="text"
-                  variant="outlined"
-                  size="small"
-                  fullWidth={true}
-                  required
-                />
-              </Grid>
-            </Grid>
-            <Button
-              sx={{
-                margin: "10px 0px",
-              }}
-              fullWidth={true}
-              type="submit"
-            >
-              Register
-            </Button>
-            <Typography>
-              Do you already have an account?{" "}
-              <Link href="/login" style={{ color: "blue" }}>
-                Login
-              </Link>
-            </Typography>
+              <Button
+                sx={{
+                  margin: "10px 0px",
+                }}
+                fullWidth={true}
+                type="submit"
+              >
+                Register
+              </Button>
+              <Typography>
+                Do you already have an account?{" "}
+                <Link href="/login" style={{ color: "blue" }}>
+                  Login
+                </Link>
+              </Typography>
+            </form>
           </Box>
         </Box>
       </Stack>
